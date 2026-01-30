50368f0a4a7fa32f258480be86b4f68052e2b879
arnabfaisal <fahimfaisalarnab@gmail.com>
2026-01-30 21:06:32 +0600

    little changes in header

--- frontend/src/components/ui/resizable-navbar.jsx
+++ frontend/src/components/ui/resizable-navbar.jsx
@@ -51,7 +51,8 @@ export const NavBody = ({
   return (
     <motion.div
       animate={{
-        backdropFilter: visible ? "blur(10px)" : "none",
+        opacity: visible ? 0.9: 1,
+        backdropFilter: "blur(10px)",
         boxShadow: visible
           ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
           : "none",
@@ -61,7 +62,7 @@ export const NavBody = ({
       transition={{
         type: "spring",
         stiffness: 200,
-        damping: 50,
+        damping: 20,
       }}
       style={{
         minWidth: "800px",
